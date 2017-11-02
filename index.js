f25f7846fc786f96dd0818f72bc202106fed0986
Mukunda Johnson <mukunda@mukunda.com>
2017-11-02 01:33:40 -0600

    jsdoc added.

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const animals = [
 
 const randomPick = source => source[Math.floor(Math.random() * source.length)];
 
+/**
+ * Picks a random animal.
+ *
+ * Animal will not be a subtype of an animal. For example, "dog" is distinct, and it will not return any other dog breeds.
+ *
+ * @returns string
+ */
 function randomAnimal() { return randomPick( animals ); }
 
 module.exports = { randomAnimal };
-
